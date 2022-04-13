bd52d5ef2a383a1a917f38c03f9c24975a2726a7
akana0321 <akana0321@gmail.com>
2022-04-13 17:29:38 +0900

    미니 프로젝트 공유

---
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.mycompany.webapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		logger.info("실행");
+//		logger.debug("확인");
+//		logger.error("에러 이유");
+		
+		
+		return "home";
+	}
+}
