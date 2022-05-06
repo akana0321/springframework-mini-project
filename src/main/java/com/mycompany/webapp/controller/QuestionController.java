7ae1440c92fb9624e408629648fed8860a44a286
akana0321 <akana0321@gmail.com>
2022-05-06 14:28:20 +0900

    Set Authority at Question Page

--- src/main/java/com/mycompany/webapp/controller/QuestionController.java
+++ src/main/java/com/mycompany/webapp/controller/QuestionController.java
@@ -49,129 +49,11 @@ public class QuestionController {
 	EstimateProcess ep;
 	
 	@RequestMapping("/questionIndex")
-	public String questionIndex() {		
-		/*//User Service Test
-		log.info(userService.getUserByUid("userid01"));
-		log.info(userService.getUserByUid("userid04"));*/
-		
-		/*//Dentist Service Test
-		log.info(dentistService.getDentistsByUid("userid23")+"\n\n\n\n");
-		log.info(dentistService.getDentistByDnumber("8889900000"));
-		// 해당 번호가 있는지 없는지 체크
-		Dentist dentist = dentistService.getDentistByDnumber("8889900000");
-		if(dentist == null) {
-			// 만약 없으면 새로 Dentist 객체를 만들고 값 세팅
-			dentist = new Dentist();
-			dentist.setDnumber("8889900000");
-			dentist.setUid("userid23");
-			dentist.setDname("치과2234");
-			dentist.setDtel("070-2321-9877");
-			dentist.setDzipcode("6666");
-			dentist.setDaddress1("서울 송파구 가락동 456");
-			dentist.setDaddress2("6층 605호");
-			dentist.setDemployees(8);
-			dentist.setDpy(90);
-			
-			// Attach 테이블에 넣기 위해 현재 마지막 Attach_no값을 가져옴
-			int lastAno = attachService.getLastAno();
-			// Attach 각각의 파일 설정
-			Attach dattach1 = new Attach();
-			dattach1.setAno(lastAno+1);	// 손수 1씩 더해줘야 함...
-			dattach1.setAttable("DENTIST");
-			dattach1.setAtid("8889900000");
-			dattach1.setAtindex("1");
-			dattach1.setAcontentType("image/png");
-			dattach1.setAsname("1231157888123-floor_plan1.png");
-			dattach1.setAoname("floor_plan1.png");
-			
-			Attach dattach2 = new Attach();
-			dattach2.setAno(lastAno+2);
-			dattach2.setAttable("DENTIST");
-			dattach2.setAtid("8889900000");
-			dattach2.setAtindex("2");
-			dattach2.setAcontentType("image/jpeg");
-			dattach2.setAsname("1232257889123-floor_plan2.jpg");
-			dattach2.setAoname("floor_plan2.png");
-			
-			// Attach List를 만들어 dentist의 dattaches에 넣음
-			List<Attach> dattachList = new ArrayList<>();
-			dattachList.add(dattach1);
-			dattachList.add(dattach2);
-			
-			dentist.setDattaches(dattachList);
-			
-			log.info(dentistService.insertDentist(dentist));
-		}*/
-		
-		/*//Product Service Test
-		log.info(productService.getProductByPid("unit-k3000b")+"\n\n");
-		log.info(productService.getProductByPid("building-house"));
-		
-		// 우선 해당 product_id가 있는지 확인
-		Product product = productService.getProductByPid("chair-macaron3");
-		log.info(product);
-		if(product == null) {
-			// 없다면 Product 객체 생성 후 값 세팅
-			product = new Product();
-			product.setPid("chair-macaron3");
-			product.setPname("macaron3");
-			product.setPcategory("CHAIR");
-			product.setPdescription("마카롱 체어 고급형");
-			product.setPprice(40);
-			product.setPcolor("black,bronwn");
-			
-			// 첨부 파일이 있을경우 현재 테이블의 마지막 Attach_no를 가져옴
-			int lastAno = attachService.getLastAno();
-			// Attach 객체 생성 후 세팅
-			Attach pattach1 = new Attach();
-			pattach1.setAno(lastAno + 1);	// Attach_no는 손수 1씩 더해줘야 함...
-			pattach1.setAttable("PRODUCT");
-			pattach1.setAtid("chair-macaron3");
-			pattach1.setAtindex("1");
-			pattach1.setAcontentType("image/png");
-			pattach1.setAsname("1234557890124-macaron3_1.png");
-			pattach1.setAoname("macaron3_1.png");
-			
-			Attach pattach2 = new Attach();
-			pattach2.setAno(lastAno + 2);
-			pattach2.setAttable("PRODUCT");
-			pattach2.setAtid("chair-macaron3");
-			pattach2.setAtindex("2");
-			pattach2.setAcontentType("image/png");
-			pattach2.setAsname("1234557890125-macaron3_2.png");
-			pattach2.setAoname("macaron3_2.png");
-			
-			// Attach List를 만들고 각각의 attach 객체를 추가한 뒤 product에 set
-			List<Attach> pattachList = new ArrayList<>();
-			pattachList.add(pattach1);
-			pattachList.add(pattach2);
-			product.setPattaches(pattachList);
-			
-			log.info(productService.insertProduct(product));
-		}*/
-		
-		/*//Question Service Test
-		log.info(questionService.getQuestionByQno(3));
-		log.info(questionService.getQuestionByQno(1));*/
-		
-		/*//Comment Service Test
-		log.info(commentService.getCommentByCno(1)+"-----------------------------------------");
-		log.info(commentService.getCommentByCno(4)+"-----------------------------------------");
-		log.info(commentService.getCommentsByQno(4)+"-----------------------------------------");*/
-		
-		/*//Attach Service Test
-		Attach target = new Attach();
-		target.setAttable("QUESTION");
-		target.setAtid("1");
-		target.setAtindex("1");
-		log.info(attachService.getAttachList(target)+"\n--------------------------------------");
-		log.info(attachService.getAttachOne(target)+"\n--------------------------------------");*/
-		
-		/*//Estimate Service Test
-		log.info(estimateService.getEstimateByEno(1)+"\n--------------------------------------");
-		log.info(estimateService.getEstimateByQno(2)+"\n--------------------------------------");
-		log.info(estimateService.getEstimateByEno(11)+"\n--------------------------------------");*/
-		
+	public String questionIndex(HttpServletRequest request) {		
+		String userId = (String) request.getSession().getAttribute("sessionUid");
+		if(userId == null) {
+			return "/question/goBackHome";
+		}
 		return "/question/questionIndex";
 	} 
 	
@@ -206,11 +88,16 @@ public class QuestionController {
 		estimate.setUid(userId);
 		estimateService.insertEstimate(estimate);
 		
-		return "/question/alert";
+		return "/question/questionFinish";
 	}
 	
 	@RequestMapping("/questionFinish")
-	public String qeustionFinish() {
+	public String questionFinish() {
 		return "redirect:/mypage/mypage";
+	}
+	
+	@RequestMapping("/goBackHome")
+	public String goBackHome() {
+		return "redirect:/home";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/question/goBackHome.jsp
@@ -0,0 +1,41 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Studio 765</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    
+    
+    <script>
+	    $(document).ready(function (){
+	        $("#myModal").modal("show");
+	    });
+    </script>
+</head>
+<body>
+<%-- modal --%>
+<div class="modal fade" tabindex="-1" id="myModal" data-backdrop="static" data-keyboard="false">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">사용 권한이 없습니다.</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>로그인 후 사용하실 수 있는 메뉴입니다.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn" onclick="location.href='goBackHome'" style="background-color: rgba(128, 128, 128, 0.614); color:white;">확인</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
